2b4d3a3867dc612310254b4c0ef8acc5434c4899
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-02-12 12:54:29 -0600

    CheckoutTest is about half-writ, just missing the button, submit, and newgative assertions

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,31 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  // Render
+  render(<CheckoutForm />);
+  // Input Queries
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  const lastNameInput = screen.getByLabelText(/last name/i);
+  const addressInput = screen.getByLabelText(/address/i);
+  const cityInput = screen.getByLabelText(/city/i);
+  const stateInput = screen.getByLabelText(/state/i);
+  const zipInput = screen.getByLabelText(/zip/i);
+  // Input Events
+  userEvent.type(firstNameInput, "Chaz");
+  userEvent.type(lastNameInput, "Chez");
+  userEvent.type(addressInput, "3632 S Chex Rd");
+  userEvent.type(cityInput, "Mixieburg");
+  userEvent.type(stateInput, "New Jefferson");
+  userEvent.type(zipInput, "36325");
+  // Button Stuff
+  // Assert
+});
